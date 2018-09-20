ae5cd7ee675fc6da3483a908f4d6da3801f79835
Ross van der Heyde <rossvdheyde@gmail.com>
2018-09-20 15:34:42 +0200

    Completed reading of input file

--- Simulate.cpp
+++ Simulate.cpp
@@ -4,8 +4,33 @@ Ross van der Heyde VHYROS001
 
 #include <iostream>
 #include <fstream>
+#include <sstream>
 #include <string>
+#include <vector>
 
+std::vector<int> parseNumbers(std::string& numbers) {
+	std::cout << "parseNumbers: " << numbers << std::endl;
+	std::vector<int> vec;
+	std::istringstream iss(numbers);
+	std::string token;
+
+	while (std::getline(iss, token, ',')) {
+		int dash = token.find("-");
+		if (dash > 0) {
+			int start = std::stoi(token.substr(0, dash));
+			int end = std::stoi(token.substr(dash + 1));
+
+			for (int i = start; i <= end; ++i) {
+				vec.push_back(i);
+			}
+
+		} else {
+			vec.push_back(std::stoi(token));
+		}
+	}
+
+	return vec;
+}
 
 int main(int argc, char const *argv[]) {
 	std::cout << "Simulate" << std::endl;
@@ -47,13 +72,26 @@ int main(int argc, char const *argv[]) {
 	std::cout << "Atoms in A: " << temp << std::endl;
 
 	//todo: parse line into numbers
+	std::vector<int> aIndx = parseNumbers(temp);
+
+	std::cout << "aIndx:" << std::endl;
+	for (std::vector<int>::iterator i = aIndx.begin(); i != aIndx.end(); ++i) {
+		std::cout << *i << std::endl;
+	}
 
 	//line 4: atoms in B
 	getline(myFile, temp);
 	std::cout << "Atoms in B: " << temp << std::endl;
+	std::vector<int> bIndx = parseNumbers(temp);
+
+	std::cout << "bIndx:" << std::endl;
+	for (std::vector<int>::iterator i = bIndx.begin(); i != bIndx.end(); ++i) {
+		std::cout << *i << std::endl;
+	}
 
 	myFile.close();
 
+	//now the fun starts
 
 	return 0;
 }
