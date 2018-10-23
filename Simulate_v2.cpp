b259b56112ea9e79f5f8eee06001cbfd7e5aacbc
Ross van der Heyde <rossvdheyde@gmail.com>
2018-10-23 21:18:40 +0200

    Memory optimisations in serial

--- Simulate_v2.cpp
+++ Simulate_v2.cpp
@@ -3,6 +3,8 @@ Ross van der Heyde VHYROS001
 5 October 2018
 reads all timestep data into a vecotr before performing calculations*/
 
+
+// #include "Simulate_v2.h"
 #include <iostream>
 #include <fstream>
 #include <sstream>
@@ -10,7 +12,7 @@ reads all timestep data into a vecotr before performing calculations*/
 #include <vector>
 #include <algorithm>
 #include <chrono>
-// #include <stdlib.h>
+#include <stdlib.h>
 #include "dcdplugin.c"
 
 std::vector<int> parseNumbers(std::string& numbers) {
@@ -57,7 +59,7 @@ struct shortPairData {
 	}
 };
 
-int main(int argc, char const *argv[]) {
+int serial(int argc, char const *argv[]) {
 	std::cout << "Simulate serial" << std::endl;
 
 	//read cmd line params
@@ -75,11 +77,6 @@ int main(int argc, char const *argv[]) {
 
 	std::cout << "input file: " << inputFile << std::endl;
 	std::cout << "output file: " << outputFile << std::endl;
-
-	//clear text from output file
-	std::ofstream outFile(outputFile);
-	outFile << "";
-	outFile.close();
 
 	//read input file
 	std::ifstream myFile(inputFile);
@@ -103,8 +100,6 @@ int main(int argc, char const *argv[]) {
 	//line 3: atoms in A
 	getline(myFile, temp);
 	std::cout << "Atoms in A: " << temp << std::endl;
-
-	//todo: parse line into numbers
 	std::vector<int> aIndx = parseNumbers(temp);
 
 	//line 4: atoms in B
@@ -116,7 +111,6 @@ int main(int argc, char const *argv[]) {
 	std::cout << "The file '" << inputFile << "' as been closed.\n" << std::endl;
 
 	//now the fun starts
-
 	//read DCD file
 	std::cout << "Reading .dcd file '" << dcdFile << "'...\n" << std::endl;
 
@@ -128,6 +122,7 @@ int main(int argc, char const *argv[]) {
 	char* fileName [] = {c_dcdFile};
 
 	int natoms = 0;
+	double initTime = getTime();
 	void* v = open_dcd_read(*fileName, "dcd", &natoms);
 	std::cout << "'" << dcdFile << "' opened" << std::endl;
 	std::cout << "natoms: " << natoms << std::endl;
@@ -136,26 +131,44 @@ int main(int argc, char const *argv[]) {
 	dcdhandle* handle = (dcdhandle*) v;
 	// std::cout << "cast" << std::endl;
 	std::cout << "frames: " << handle->nsets << std::endl;
+	int totalTimesteps = handle->nsets;
 
 	//read timesteps
 	//read timesteps. this seems very inefficient. uses a lot of memory. may its just my weak laptop
-	std::vector<molfile_timestep_t> timesteps;
-	for (int timestepCounter = 0; timestepCounter < handle->nsets; ++timestepCounter) {
+	// std::vector<molfile_timestep_t> timesteps;
+	// molfile_timestep_t* timesteps = new molfile_timestep_t[totalTimesteps];
+
+	float** timestepsData = new float*[totalTimesteps];
+
+	for (int timestepCounter = 0; timestepCounter < totalTimesteps; ++timestepCounter) {
 		// std::cout << "timestep: " << timestepCounter << std::endl;
+		timestepsData[timestepCounter] = new float[3 * natoms];
 		molfile_timestep_t timestep; // data for the current timestep
 		timestep.coords = (float *)malloc(3 * sizeof(float) * natoms); //change to normal array?
 		int rc = read_next_timestep(v, natoms, &timestep);
 
-		timesteps.push_back(timestep);
+		// timesteps[timestepCounter] = timestep;
+		memcpy(timestepsData[timestepCounter], timestep.coords, 3 * natoms * sizeof(float));
+		delete[] timestep.coords;
 	}
 
+	//close file
+	std::cout << "Closing file '" << dcdFile << "'" << std::endl;
+	close_file_read(v);
+	std::cout << ".dcd file closed" << std::endl;
+	delete[] c_dcdFile;
 
+	double time = (getTime() - initTime) / 1000;
+	std::cout << "Time to read .dcd file: " << time << " s" << std::endl;
+
+
+	initTime = getTime();
 	std::vector<shortPairData> writeToFile;// e.g. "0,304,168043,14.23986456"
-	double initTime = getTime();
+
 	// for (int timestepCounter = 0; timestepCounter < 10; timestepCounter++) {//REMOVE THIS WHEN NOT TESTING
-	for (int timestepCounter = 0; timestepCounter < handle->nsets; timestepCounter++) {
-		std::cout << "timestep: " << timestepCounter << std::endl;
-		const molfile_timestep_t timestep = timesteps[timestepCounter];
+	for (int timestepCounter = 0; timestepCounter < totalTimesteps; timestepCounter++) {
+		// std::cout << "timestep: " << timestepCounter << std::endl;
+		// const molfile_timestep_t timestep = timesteps[timestepCounter];
 
 		//vector of distances
 		std::vector<std::pair<std::string, double> > distances;
@@ -166,18 +179,25 @@ int main(int argc, char const *argv[]) {
 			float ax, ay, az;
 			int a = *aIt;
 			// std::cout << "a atom: " << a << std::endl;
-			ax = timestep.coords[3 * a];
+			/*ax = timestep.coords[3 * a];
 			ay = timestep.coords[(3 * a) + 1];
-			az = timestep.coords[(3 * a) + 2];
+			az = timestep.coords[(3 * a) + 2];*/
+
+			ax = timestepsData[timestepCounter][3 * a];
+			ay = timestepsData[timestepCounter][(3 * a) + 1];
+			az = timestepsData[timestepCounter][(3 * a) + 2];
 
 			for (std::vector<int>::iterator bIt = bIndx.begin(); bIt != bIndx.end(); ++bIt) {
 				//coords of atom
 				float bx, by, bz;
 				int b = *bIt;
 				// std::cout << "b atom: " << b << std::endl;
-				bx = timestep.coords[3 * b];
+				/*bx = timestep.coords[3 * b];
 				by = timestep.coords[(3 * b) + 1];
-				bz = timestep.coords[(3 * b) + 2];
+				bz = timestep.coords[(3 * b) + 2];*/
+				bx = timestepsData[timestepCounter][3 * b];
+				by = timestepsData[timestepCounter][(3 * b) + 1];
+				bz = timestepsData[timestepCounter][(3 * b) + 2];
 
 				double dist = sqrt(pow((ax - bx), 2) + pow((ay - by), 2) + pow((az - bz), 2));
 
@@ -190,12 +210,13 @@ int main(int argc, char const *argv[]) {
 			}
 		}
 
+		delete[] timestepsData[timestepCounter];
+
 		//sort based on distance
 		//You don't need to sort. Just go through array and keep k shortest distances
 		std::partial_sort(distances.begin(), distances.begin() + k, distances.end(),
 		                  [](const std::pair<std::string, double>& lhs, const std::pair<std::string, double>& rhs)
 		                  ->bool{return lhs.second <= rhs.second;});
-		//maybe use dv&c algorithm k times, each time discarding the closest pair?
 
 		// add k distances to writeToFile
 		for (int j = 0; j < k; j++) {
@@ -206,16 +227,17 @@ int main(int argc, char const *argv[]) {
 			writeToFile.push_back(spd);
 		}
 
-		delete[] timestep.coords;//not sure if this is necessary
+		// delete[] timestep.coords;//not sure if this is necessary
 	}
-	//close file
-	std::cout << "Closing file '" << dcdFile << "'" << std::endl;
-	close_file_read(v);
-	std::cout << ".dcd file closed" << std::endl;
-	delete[] c_dcdFile;
+	// delete[] timesteps;
+	delete[] timestepsData;
 
+	time = (getTime() - initTime) / 1000;
+	std::cout << "Time to perform calcualtions: " << time << " s" << std::endl;
+
+	initTime = getTime();
 	std::cout << "writing to output file '" << outputFile << "' " << std::endl;
-	outFile.open(outputFile);
+	std::ofstream outFile(outputFile);
 	outFile.precision(15);
 
 	std::cout << "writeToFile.size: " << writeToFile.size() << std::endl;
@@ -224,10 +246,21 @@ int main(int argc, char const *argv[]) {
 	}
 
 	outFile.close();
-	double time = getTime() - initTime;
+	time = (getTime() - initTime) / 1000;
+	std::cout << "Time to write to output file: " << time << " s" << std::endl;
 	std::cout << "writing output complete" << std::endl;
-	std::cout << "Total time: " << (time / 1000) << " s" << std::endl;
+	// std::cout << "Total time: " << (time / 1000) << " s" << std::endl;
 
 	std::cout << "Complete" << std::endl;
+	return 0;
+}
+
+int main(int argc, char const *argv[]) {
+	/*for (size_t i = 0; i < 10; i++) {
+		std::cout << "serial run " << i << '\n';*/
+
+	serial(argc, argv);
+	// std::cout << "\n" << '\n';
+	// }
 	return 0;
 }
--- makefile
+++ makefile
@@ -2,105 +2,47 @@
 # Ross van der Heyde VHYROS001
 # 17 September 2018
 
-# serial version -------------------------------------------------
-serial: Simulate.o
-	g++ -g -o Simulate Simulate.o -std=c++11
-
-Simulate.o: Simulate.cpp dcdplugin.c
-	g++ -c -g -o Simulate.o Simulate.cpp -std=c++11
-
-debug: Simulate
-	gdb Simulate.exe
-
-runserial: Simulate
-	 ./Simulate -i example_input_file1.txt -o serialOutput1.txt
-# ./Simulate -i testInput.txt -o testOutput.txt
-
-# serial version that reads all timestep data once--------------
-serialv2: Simulate_v2.o
+# serial version that reads all timestep data at once--------------
+serial: Simulate_v2.o
 	g++ -g -o Simulate_v2 Simulate_v2.o -std=c++11
 
 Simulate_v2.o: Simulate_v2.cpp dcdplugin.c
 	g++ -c -g -o Simulate_v2.o Simulate_v2.cpp -std=c++11
 
-runserialv2: serialv2
-	 ./Simulate_v2 -i example_input_file1.txt -o serial_v2_Output1.txt
+runserial: serial
+	 ./Simulate_v2 -i example_input_file1.txt -o serial_Output.txt
 
-# openmp version parallelized at a ---------------
-openmp: SimulateOMP.o
-	g++ -g -fopenmp -o SimulateOMP SimulateOMP.o -std=c++11
-
-SimulateOMP.o: SimulateOMP.cpp dcdplugin.c
-	g++ -c -g -fopenmp -o SimulateOMP.o SimulateOMP.cpp -std=c++11
-
-runopenmp: openmp
-	./SimulateOMP -i example_input_file1.txt -o openMPoutput.txt 
+cleanserial:
+	rm Simulate_v2.o
+	rm Simulate_v2
 
 #openp version parallelized at timestep------------------------
-openmptimestep: omp_v2.o
+openmp: omp_v2.o
 	g++ -g -fopenmp -o omp_v2 omp_v2.o -std=c++11
 
 omp_v2.o: omp_v2.cpp dcdplugin.c
 	g++ -c -g -fopenmp -o omp_v2.o omp_v2.cpp -std=c++11
 
-runopenmptimestep: openmptimestep
+runopenmp: openmptimestep
 	./omp_v2 -i example_input_file1.txt -o openMPTimestepOutput.txt
 
-#MPI -------------------------------------------------------------------
-runmpi: mpiCPP
-	mpiexec -n 4 my_mpiCPP -i example_input_file1.txt -o mpiOutput.txt
+cleanopenmp:
+	rm omp_v2.o
+	rm omp_v2
 
-mpiCPP: mpiCPP.o
+#MPI -------------------------------------------------------------------
+mpi: mpiCPP.o
 	mpic++ -lm -g -o my_mpiCPP my_mpiCPP.o
 
 mpiCPP.o: mpiCPP.cpp
 	mpic++ -lm -g -c -o my_mpiCPP.o mpiCPP.cpp -std=c++11
 
+runmpi: mpi
+	mpiexec -n 4 my_mpiCPP -i example_input_file1.txt -o mpiOutput.txt
+
 debugmpi: mpiCPP
 	gdb --args my_mpiCPP -i example_input_file1.txt -o mpiOutput.txt
 
 cleanmpi:
-	rm *.o
+	rm my_mpiCPP.o
 	rm my_mpiCPP
-
-# ---------------------------
-runmpi_2: mpiCPP_2
-	mpiexec -n 4 my_mpiCPP -i example_input_file1.txt -o mpiOutput.txt
-
-mpiCPP_2: mpiCPP_2.o
-	mpic++ -lm -g -o my_mpiCPP my_mpiCPP.o
-
-mpiCPP_2.o: mpiCPP_2.cpp
-	mpic++ -lm -g -c -o my_mpiCPP.o mpiCPP_2.cpp -std=c++11
-
-# mpi: library my_mpi.o
-# 	mpiCC -lm -g -o my_mpi my_mpi.o -I ./library/ -L ./library/ -llibrary
-
-# library: library.o
-# 	g++ -lm -g -o ./library/liblibrary.so ./library/library.o -fPIC -shared -std=c++11
-
-# library.o: ./library/library.cpp
-# 	g++ -lm -g -c -o ./library/library.o ./library/library.cpp -fPIC -shared -std=c++11
-
-# my_mpi.o: my_mpi.c
-# 	mpiCC -lm -g -c -o my_mpi.o my_mpi.c -I ./library/ -L ./library/ -llibrary
-
-# runmpi: mpi
-# 	export LD_LIBRARY_PATH=library/ && mpiexec -n 4 my_mpi -i example_input_file1.txt -o mpiOutput.txt
-
-# debugmpi:
-# 	export LD_LIBRARY_PATH=library/ && gdb --args my_mpi -i example_input_file1.txt -o mpiOutput.txt
-# ---------------------------------------------------------------------
-test: fileTest.c
-	gcc -g -o fileTest fileTest.c
-
-runtest: test
-	./fileTest -i example_input_file1.txt
-
-
-# ---------------------------------------------------------------------
-clean:
-	rm *.o
-	rm Simulate
-	rm my_mpi
-#	rm *.exe
